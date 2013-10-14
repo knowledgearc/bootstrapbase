e63e838d2a7990ccda4a8aee4f96ad308d2b0c7a
Hayden Young <haydenyoung@wijiti.com>
2013-10-14 19:43:51 +0800

    Added jquery. Moved bootstrap to jui so it is loaded by Joomla core.

--- templates/bootstrapbase/variables.php
+++ templates/bootstrapbase/variables.php
@@ -1,6 +1,21 @@
 <?php
 defined('_JEXEC') or die;
 
+$application = JFactory::getApplication();
+$document = JFactory::getDocument();
+
+$option = $application->input->getCmd('option', '');
+$view = $application->input->getCmd('view', '');
+$layout = $application->input->getCmd('layout', '');
+$task = $application->input->getCmd('task', '');
+$itemid = $application->input->getCmd('Itemid', '');
+
+// @todo Not sure why we set the template with the JDocument language and 
+// direction because the template is a fully initiated JDocumentHTML 
+// object as well.
+$this->language = $document->language;
+$this->direction = $document->direction;
+
 // Set the body class for the overall web page.
 $bodyClass = ' '.$option.' view-'. $view.
 	($layout ? ' layout-' . $layout : ' no-layout').
