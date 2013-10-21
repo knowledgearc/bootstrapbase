03160ce93a66a043935b8a86e3be87eb40391518
Hayden Young <haydenyoung@wijiti.com>
2013-10-22 03:17:19 +0800

    Basic layout with mixins.

--- templates/bootstrapbase/index.php
+++ templates/bootstrapbase/index.php
@@ -13,26 +13,41 @@ $this->addStylesheet(JURI::base().'/templates/'.$this->template.'/css/template.m
 		<jdoc:include type="head" />
 	</head>
 
-	<body class="<?php $bodyClass; ?>">
-		<jdoc:include type="module" name="menu" title="Main Menu" />
-		<jdoc:include type="message" />
-		
-		<jdoc:include type="modules" name="header" />
-		<jdoc:include type="module" name="hero" title="Hero" />
-		<jdoc:include type="modules" name="top" />
-		
-		<jdoc:include type="modules" name="above-content" />
-		<jdoc:include type="module" name="breadcrumbs" title="Breadcrumbs" />		
-		<jdoc:include type="component" />		
-		<jdoc:include type="modules" name="below-content" />
-		
-		<jdoc:include type="modules" name="left" />
-		<jdoc:include type="modules" name="right" />
-		
-		<jdoc:include type="modules" name="bottom" />
-		<jdoc:include type="modules" name="footer" />
-		
-		<jdoc:include type="modules" name="debug" />		
+	<body<?php $bodyClass ? ' class="'.$bodyClass.'"' : ''; ?>>
+		<div class="container">
+			<header>
+				<jdoc:include type="module" name="menu" title="Main Menu" />
+				<jdoc:include type="message" />
+			
+				<jdoc:include type="modules" name="header" />
+				<jdoc:include type="module" name="hero" title="Hero" />
+				<jdoc:include type="modules" name="top" />
+			</header>
+			
+			<main role="main">
+				<aside class="sidebar">
+					<jdoc:include type="modules" name="left" />
+				</aside>
+	
+				<div id="mainComponent">
+					<jdoc:include type="modules" name="above-content" />
+					<jdoc:include type="module" name="breadcrumbs" title="Breadcrumbs" />		
+					<jdoc:include type="component" />		
+					<jdoc:include type="modules" name="below-content" />			
+				</div>
+	
+				<aside class="sidebar">
+					<jdoc:include type="modules" name="right" />
+				</aside>
+			</main>
+			
+			<footer>
+				<jdoc:include type="modules" name="bottom" />
+				<jdoc:include type="modules" name="footer" />
+			
+				<jdoc:include type="modules" name="debug" />
+			</footer>
+		</div>		
 	</body>
 </html>
 		
