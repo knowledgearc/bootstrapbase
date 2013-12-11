1a04b1f194e5d94e3fa35781ad55233b0e455b8c
Hayden Young <haydenyoung@wijiti.com>
2013-12-12 02:03:59 +0800

    Breadcrumbs override integration with Bootstrap.

--- 3rd_party_extension_overrides/jsolrsearch/com_jsolrsearch/basic/results_form.php
+++ 3rd_party_extension_overrides/jsolrsearch/com_jsolrsearch/basic/results_form.php
@@ -5,9 +5,8 @@
  * 
  * Copy this file to override the layout and style of the search results form.
  * 
- * @package		JSolr
- * @subpackage	Search
  * @copyright	Copyright (C) 2012-2013 Wijiti Pty Ltd. All rights reserved.
+ * @copyright	Copyright (C) 2013 KnowledgeARC Ltd. All rights reserved.
  * @license     This file is part of the JSolrSearch Component for Joomla!.
 
    The JSolrSearch Component for Joomla! is free software: you can redistribute it 
