9551f9213c6f6f3d25a3dd32455f6e4674a792db
Hayden Young <haydenyoung@wijiti.com>
2014-02-10 00:33:26 +0800

    Optionally load a brand from a custom module.

--- templates/bootstrapbase/html/mod_menu/navbar.php
+++ templates/bootstrapbase/html/mod_menu/navbar.php
@@ -17,70 +17,84 @@ if ($params->get('tag_id') != null) {
 } else {
 	$target = 'navbar-'.$module->id.'-collapse';
 }
+
+$logo = null;
+$brands = JModuleHelper::getModules('navbar-brand');
+
+if (count($brands)) {	
+	$brand = JArrayHelper::getValue($brands, 0);
+	$renderer = JFactory::getDocument()->loadRenderer('module');
+	$logo = $renderer->render($brand);
+}
 ?>
 <nav<?php echo $tag; ?> class="navbar <?php echo $class_sfx; ?>" role="navigation">
-	<div class="navbar-header">
-		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".<?php echo $target; ?>">
-			<span class="sr-only">Toggle navigation</span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</button>
-	</div>
-
-	<div class="collapse navbar-collapse <?php echo $target; ?>">
-		<ul class="nav navbar-nav">
-			<?php
-			foreach ($list as $i => &$item) {
-				$class = 'item-'.$item->id;
-				
-				if ($item->id == $active_id) {
-					$class .= ' current';
-				}
-				
-				if (in_array($item->id, $path)) {
-					$class .= ' active';
-				} elseif ($item->type == 'alias') {
-					$aliasToId = $item->params->get('aliasoptions');
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".<?php echo $target; ?>">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<?php if ($logo) : ?>
+			<a class="navbar-brand" href="<?php echo JUri::base(); ?>"><?php echo $logo; ?></a>
+			<?php endif; ?>
+		</div>
+	
+		<div class="collapse navbar-collapse <?php echo $target; ?>">
+			<ul class="nav navbar-nav">
+				<?php
+				foreach ($list as $i => &$item) {
+					$class = 'item-'.$item->id;
 					
-					if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+					if ($item->id == $active_id) {
+						$class .= ' current';
+					}
+					
+					if (in_array($item->id, $path)) {
 						$class .= ' active';
-					} elseif (in_array($aliasToId, $path)) {
-						$class .= ' alias-parent-active';
+					} elseif ($item->type == 'alias') {
+						$aliasToId = $item->params->get('aliasoptions');
+						
+						if (count($path) > 0 && $aliasToId == $path[count($path) - 1]) {
+							$class .= ' active';
+						} elseif (in_array($aliasToId, $path)) {
+							$class .= ' alias-parent-active';
+						}
+					}
+					
+					if (!empty($class)) {
+						$class = ' class="'.trim($class) .'"';
+					}
+				
+					echo '<li'.$class.'>';
+	
+					// Render the menu item.
+					switch ($item->type) {
+						case 'separator':
+						case 'url':
+						case 'component':
+						case 'heading':
+							require JModuleHelper::getLayoutPath('mod_menu', 'navbar_'.$item->type);
+							break;
+					
+						default:
+							require JModuleHelper::getLayoutPath('mod_menu', 'navbar_url');
+							break;
+					}
+	
+					// The next item is deeper.
+					if ($item->deeper) {
+						echo '<ul class="dropdown-menu">';
+					} elseif ($item->shallower) {
+						echo '</li>';
+						echo str_repeat('</ul></li>', $item->level_diff);
+					} else {
+						echo '</li>';
 					}
 				}
-				
-				if (!empty($class)) {
-					$class = ' class="'.trim($class) .'"';
-				}
-			
-				echo '<li'.$class.'>';
-
-				// Render the menu item.
-				switch ($item->type) {
-					case 'separator':
-					case 'url':
-					case 'component':
-					case 'heading':
-						require JModuleHelper::getLayoutPath('mod_menu', 'navbar_'.$item->type);
-						break;
-				
-					default:
-						require JModuleHelper::getLayoutPath('mod_menu', 'navbar_url');
-						break;
-				}
-
-				// The next item is deeper.
-				if ($item->deeper) {
-					echo '<ul class="dropdown-menu">';
-				} elseif ($item->shallower) {
-					echo '</li>';
-					echo str_repeat('</ul></li>', $item->level_diff);
-				} else {
-					echo '</li>';
-				}
-			}
-			?>
-		</ul>
+				?>
+			</ul>
+		</div>
 	</div>
 </nav>
