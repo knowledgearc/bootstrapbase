fbc94a7bae0fb34fd7ef43d1cc83c36c251eada8
haydenyoung <hayden@knowledgearc.com>
2016-04-23 18:09:30 +0800

    Bootstrapified module chrome.

---
+++ templates/bootstrapbase/html/modules.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     BootstrapBase
+ * @subpackage  Template
+ *
+ * @copyright   Copyright (C) 2013-2016 KnowledgeArc Ltd. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+/*
+ * The default Boostrapbase chrome. Uses the module manager Advanced settings
+ * to fully control the module wrapper.
+ */
+function modChrome_bootstrapified($module, &$params, &$attribs) {
+    $moduleTag = $params->get('module_tag', 'div');
+    $headerTag = htmlspecialchars($params->get('header_tag', 'h3'));
+    $bootstrapSize = (int) $params->get('bootstrap_size', 0);
+    $moduleClasses = array();
+
+    if ($params->get('moduleclass_sfx')) {
+        $moduleClasses[] = htmlspecialchars($params->get('moduleclass_sfx'));
+    }
+
+    if ($bootstrapSize != 0) {
+        $moduleClasses[] = 'col-md-'.$bootstrapSize;
+    }
+
+    $moduleClass = count($moduleClasses) > 0 ? " class=\"".implode(" ", $moduleClasses)."\"" : "";
+
+    // Temporarily store header class in variable
+    $headerClass = $params->get('header_class');
+    $headerClass = !empty($headerClass) ? ' class="'.htmlspecialchars($headerClass).'"' : '';
+
+    if (!empty($module->content)) : ?>
+        <<?php echo $moduleTag; ?><?php echo $moduleClass; ?>>
+
+        <?php if ((bool) $module->showtitle) :?>
+            <<?php echo $headerTag.$headerClass.'>' . $module->title; ?></<?php echo $headerTag; ?>>
+        <?php endif; ?>
+
+        <?php echo $module->content; ?>
+
+        </<?php echo $moduleTag; ?>>
+    <?php endif;
+}
--- templates/bootstrapbase/index.php
+++ templates/bootstrapbase/index.php
@@ -25,29 +25,29 @@ require_once (JPATH_THEMES.'/'.$this->template.'/initialize.php');
                 <jdoc:include type="message" />
 
                 <div id="header-modules">
-                    <jdoc:include type="modules" name="header" />
+                    <jdoc:include type="modules" name="header" style="bootstrapified" />
                 </div>
 
                 <div id="hero-modules">
-                    <jdoc:include type="modules" name="hero" />
+                    <jdoc:include type="modules" name="hero" style="bootstrapified" />
                 </div>
             </div>
         </header>
 
         <div id="top">
-            <jdoc:include type="modules" name="top" />
+            <jdoc:include type="modules" name="top" style="bootstrapified" />
         </div>
 
         <div id="content">
             <?php if (array_search($mainClass, array("left-sidebar", "both-sidebars")) !== false) : ?>
             <aside id="sidebar1" class="sidebar">
-                <jdoc:include type="modules" name="left" />
+                <jdoc:include type="modules" name="left" style="bootstrapified" />
             </aside>
             <?php endif; ?>
 
             <main role="main" class="<?php echo $mainClass; ?>">
                 <div id="above-content-modules">
-                    <jdoc:include type="modules" name="above-content" />
+                    <jdoc:include type="modules" name="above-content" style="bootstrapified" />
                 </div>
 
                 <jdoc:include type="module" name="breadcrumbs" title="Breadcrumbs" />
@@ -55,25 +55,25 @@ require_once (JPATH_THEMES.'/'.$this->template.'/initialize.php');
                 <jdoc:include type="component" />
 
                 <div id="below-content-modules">
-                    <jdoc:include type="modules" name="below-content" />
+                    <jdoc:include type="modules" name="below-content" style="bootstrapified" />
                 </div>
             </main>
 
             <?php if (array_search($mainClass, array("right-sidebar", "both-sidebars")) !== false) : ?>
             <aside id="sidebar2" class="sidebar">
-                <jdoc:include type="modules" name="right" />
+                <jdoc:include type="modules" name="right" style="bootstrapified" />
             </aside>
             <?php endif; ?>
         </div>
 
         <div id="bottom">
-            <jdoc:include type="modules" name="bottom" />
+            <jdoc:include type="modules" name="bottom" style="bootstrapified" />
         </div>
 
         <footer>
             <div id="footer-container">
                 <div id="footer-modules">
-                    <jdoc:include type="modules" name="footer" />
+                    <jdoc:include type="modules" name="footer" style="bootstrapified" />
                 </div>
             </div>
 
