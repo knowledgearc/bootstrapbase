47a7f63e06cb42d29b777fc049fa3a28b87d94db
haydenyoung <hayden@knowledgearc.com>
2016-04-23 18:04:57 +0800

    Update copyright date.

--- templates/bootstrapbase/libraries/bootstrapbase/document/html/renderer/html/js.php
+++ templates/bootstrapbase/libraries/bootstrapbase/document/html/renderer/html/js.php
@@ -3,7 +3,7 @@
  * @package     Bootstrapbase
  * @subpackage  Document
  *
- * @copyright   Copyright (C) 2013-2015 KnowledgeArc Ltd. All rights reserved.
+ * @copyright   Copyright (C) 2013-2016 KnowledgeArc Ltd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
  */
 
